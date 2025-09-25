8ca1764427403a5b90a66b020814be0771ece478
phillipdelphias <phillipdelphias@gmail.com>
2025-09-25 17:27:38 +1000

    Attempt to fix more build errors?

--- src/app/games/[slug]/page.tsx
+++ src/app/games/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import fs from "fs";
 import path from "path";
 import GameFrame from "@/lib/games/frames/gameFrame";
+import { AArrowUp } from "lucide-react";
 
 type PageProps = {
     params: {
@@ -29,14 +30,15 @@ async function getGameData(slug: string) {
     const gameDir = path.join(process.cwd(), "public/raw/games", slug);
     const filePath = path.join(gameDir, "game.json");
     const fileContents = fs.readFileSync(filePath, "utf-8");
-    return JSON.parse(fileContents);
+    return { id: slug, ...JSON.parse(fileContents) };
 }
 
 export default async function GamePage({ params }: PageProps) {
+    await params;
     const game = await getGameData(params.slug);
     return (
         <div className="p-5">
-            <GameFrame params={params} game={game} />
+            <GameFrame game={game} />
             <p>{game.description}</p>
         </div>
     );
--- src/lib/games/frames/gameFrame.tsx
+++ src/lib/games/frames/gameFrame.tsx
@@ -4,17 +4,15 @@ import { Maximize } from "lucide-react";
 
 type game = {
     name: string,
-    categories: string
-    description: string
+    categories: string,
+    description: string,
+    id: string
 }
 type frameParams = {
-    params: {
-        slug: string
-    },
     game: game
 }
 
-export default function GameFrame({ params, game }: frameParams) {
+export default function GameFrame({ game }: frameParams) {
 
     const iframe = useRef<HTMLIFrameElement>(null);
     
@@ -22,9 +20,9 @@ export default function GameFrame({ params, game }: frameParams) {
     return (
 
             <div className="bg-main-800 flex flex-col rounded-4xl overflow-hidden">
-                <iframe src={`/raw/games/${params.slug}/index.html`} className="w-full aspect-video" title={game.name} ref={iframe}></iframe>
+                <iframe src={`/raw/games/${game.id}/index.html`} className="w-full aspect-video" title={game.name} ref={iframe}></iframe>
                 <div className="grow p-5 flex flex-row justify-between">
-                    <h1 className="text-xl">{game.name ?? params.slug}</h1>
+                    <h1 className="text-xl">{game.name ?? game.id}</h1>
                     <button title="Fullscreen" className="hover:bg-white/50 p-2 rounded-[50%] transition-all hover:cursor-pointer"
                         onClick={() => iframe.current?.requestFullscreen()}
                     >
